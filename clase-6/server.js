df457bb909de2c93d795e1f0ffd4fad41fa778a3
lucas <lucasperezfernandez@gmail.com>
2022-08-12 17:31:11 -0300

    cambios en server.js

--- clase-6/server.js
+++ clase-6/server.js
@@ -1,11 +1,22 @@
-const express = require('express')
+const express = require('express');
+const fs = require('fs');
+const { title } = require('process');
 
 const app = express()
-
+const productos = fs.readFileSync('./src/productos.txt')
 app.get('/', (request, response)=>{
     response.send('Servidor iniciado con express!')
 })
 
-const server = app.listen(8080, ()=>{
+app.get('/productos', (request, response)=>{
+    response.send(`${productos}`)
+})
+
+app.get('/productosRandom', (request, response)=>{
+    response.send()
+})
+
+const PORT =8080
+const server = app.listen(PORT, ()=>{
     console.log(`Servidor http escuchando en http://localhost:8080/`)
 })
