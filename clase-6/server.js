e9afe52539f0140824a82287cfbee2c67f91c7ed
Lucas Perez Fernandez <lucasperezfernandez@hotmail.com>
2022-08-22 21:03:35 -0300

    cambios

--- clase-6/server.js
+++ clase-6/server.js
@@ -2,8 +2,25 @@ const express = require('express');
 const fs = require('fs');
 const { title } = require('process');
 
+
 const app = express()
-const productos = fs.readFileSync('./src/productos.txt')
+const productos = JSON.parse(fs.readFileSync('./src/prodcutos.json', 'utf-8'))
+class Prod{
+    constructor(titulo,precio){
+        this.titulo = titulo
+        this.precio = precio
+    }
+    listarAll(){
+        const objs = JSON.parse(fs.readFileSync(this.titulo, 'utf-8'))
+        return [];
+    }
+}
+
+
+
+
+
+// PAGINAS SERVER Y LO QUE MUESTRAN
 app.get('/', (request, response)=>{
     response.send('Servidor iniciado con express!')
 })
@@ -16,7 +33,11 @@ app.get('/productosRandom', (request, response)=>{
     response.send()
 })
 
+// SERVER
 const PORT =8080
 const server = app.listen(PORT, ()=>{
     console.log(`Servidor http escuchando en http://localhost:8080/`)
 })
+
+const prod = new Prod('./src/prodcutos.json')
+console.log(productos)
